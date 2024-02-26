9f3117a71e79ed71ddf59c644539218215103c92
Sinval Luz <contato.sinvalluz@gmail.com>
2024-02-26 09:59:47 -0300

    Adicionando funções e condicionais que verificam a opção escolhida

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
-print('''
+import os
+
+def exibir_nome_do_programa():
+    print('''
 ██████████████████████████████████████████████████████████████████████████
 █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
 █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
 ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
 
      
-''');
+''')
 
-print('1. Cadastrar restaurante');
-print('2. Listar restaurante');
-print('3. Ativar restaurante');
-print('4. Sair\n');
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ');
-print(f'Você escolheu a opção {opcao_escolhida}' );
+
+def finalizar_app():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+        
+    print('Finalizando app\n')
+
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
